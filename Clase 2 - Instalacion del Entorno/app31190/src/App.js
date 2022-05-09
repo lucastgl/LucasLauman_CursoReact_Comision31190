bdb6f5a664dbf4ca7e278674f0207b040747c29b
lucastgl <lucastgarcialauman@gmail.com>
2022-05-09 20:37:40 -0300

    personalización de App.js

--- Clase 2 - Instalacion del Entorno/app31190/src/App.js
+++ Clase 2 - Instalacion del Entorno/app31190/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const AvatarJSX = () => {
+  const src = `https://randomuser.me/api/portraits/lego/1.jpg`
+
+  return (
+      <picture>
+          <img src={src} alt='Lego 1'/>
+          Mauricio
+      </picture>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        < AvatarJSX />
+        < AvatarJSX />
       </header>
     </div>
   );
