a6889ba60c2168eb2b5cec7a65a8c119d657067c
lucastgl <lucastgarcialauman@gmail.com>
2022-05-16 00:07:26 -0300

    navbar con estilos para entrega

--- Clase 2 - Instalacion del Entorno/app31190/src/components/navbar/NavBar.js
+++ Clase 2 - Instalacion del Entorno/app31190/src/components/navbar/NavBar.js
@@ -4,14 +4,21 @@ import logo from './logo.svg';
 const NavBar = () => {
     return (
         <nav className="NavBar">
-            <img src={logo} className="App-logo" alt="logo"></img>
-            <button className="botonNavBar">INICIO</button>
-            <ul className="Listado">
-                <li>Sneakers</li>
-                <li>Hodies</li>
-                <li>Joggers</li>
-            </ul>
-            <button className="botonNavBar">LOGIN</button>
+            <div className="reactHead">
+                <img src={logo} className="App-logo" alt="logo"></img>
+                <p className="presentation">Powered by React</p>
+            </div>
+            <div className="elementsHead">
+                <ul className="Listado" type="none">
+                    <li>Sneakers</li>
+                    <li>Hodies</li>
+                    <li>Joggers</li>
+                </ul>
+                <div className='divBotones'>
+                    <button className="botonNavBar">INICIO</button>
+                    <button className="botonNavBar">LOGIN</button>
+                </div>
+            </div>
         </nav>
     )
 }
